508f2d3e6ba82ae290add5a686d81733f92493bc
App Academy Student <email@email.com>
2020-02-10 18:10:12 -0500

    Hopefully final final pre-Heroku commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def current_user
-    return nil unless session[:session_token]
-    @current_user || User.find_by(session_token: (session[:session_token])
-  end
+  # def current_user
+  #   return nil unless session[:session_token]
+  #   @current_user || User.find_by(session_token: (session[:session_token])
+  # end
 
   def logged_in?
     !!current_user
