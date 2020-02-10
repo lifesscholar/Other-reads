1dee78fc86aa21038aa7f08e449f495eb9d9efa7
App Academy Student <email@email.com>
2020-02-10 18:29:07 -0500

    Final final FINAL pre-heroku commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :email, :session_token, :password_digest, presence: true
   validates :email, :session_token, uniqueness: true
-  validates :password, length: { minumum: 6 }, allow_nil: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
 
   after_initialize :ensure_session_token
 
