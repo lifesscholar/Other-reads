9845680de084870117385c64812c7b8f9fe56168
App Academy Student <email@email.com>
2020-02-10 18:59:21 -0500

    Finished Users model and Users Controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,9 +3,16 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
   
     if @user.save
+      login(@user)
+      render "api/users/show"
     else
       render json: @user.errors.full_messages, status: 422
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render "api/users/show"
   end
 
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # def current_user
-  #   return nil unless session[:session_token]
-  #   @current_user || User.find_by(session_token: (session[:session_token])
-  # end
+  def current_user
+    return nil unless session[:session_token]
+    @current_user || User.find_by(session_token: (session[:session_token]))
+  end
 
   def logged_in?
     !!current_user
@@ -16,8 +16,19 @@ class ApplicationController < ActionController::Base
   def login(user)
     user.reset_session_token!
     session[:session_token] = user.session_token
+    @current_user = user
   end
 
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
 
+  def require_logged_in
+    unless current_user
+      render json: { base: ['invalid credentials'] }, status: 401
+    end
+  end
 
 end
