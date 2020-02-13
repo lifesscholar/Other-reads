1ac4fd4547966d3a107c8191144984c43c3c6e79
App Academy Student <email@email.com>
2020-02-13 18:03:09 -0500

    Started working on MVP-2

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,3 +1,12 @@
 class Api::BooksController < ApplicationController
-  
+
+  def index
+    @books = Book.all
+    render :index
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
 end
